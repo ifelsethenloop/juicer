18bed9d195fb1601050f58149eefdd11e22af274
Alek Petkovski <bairointernet@gmail.com>
2022-02-22 23:23:16 +1100

    current base added

--- juicer.py
+++ juicer.py
@@ -5,5 +5,14 @@ current = int(input('Current nicotine concentration of E-liquid (mg/ml): '))
 nic_base = int(input('Nicotine base strength (mg/ml): '))
 target = int(input('Desired nicotine strength (mg/ml): '))
 
-print(' ({}ml bottle) / ({}mg/ml nicotine strength) x ({}mg/ml nicotine target strength) = '.format(amount, nic_base, target))
-print(amount / nic_base * target,'ml')
+target_converted = target - current
+
+# print(' ({}ml bottle) / ({}mg/ml nicotine strength) x ({}mg/ml nicotine target strength) = '.format(amount, nic_base, target))
+# print(amount / nic_base * target,'ml')
+
+if current == 0:
+    print(' ({}ml bottle) / ({}mg/ml nicotine strength) x ({}mg/ml nicotine target strength) = '.format(amount, nic_base, target))
+    print(amount / nic_base * target,'ml')
+else:
+    print(' ({}ml bottle) / ({}mg/ml nicotine strength) x ({}mg/ml nicotine target strength) = '.format(amount, nic_base, target_converted))
+    print(amount / nic_base * target_converted, 'ml')
