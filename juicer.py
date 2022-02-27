97473be6c62c5eb66850ca094fe244e8e5cd0b80
Alek Petkovski <bairointernet@gmail.com>
2022-02-27 23:16:35 +1100

    calc improved

--- juicer.py
+++ juicer.py
@@ -1,20 +1,30 @@
 # E-juice nicotine mixing calculator
+import math
 
-amount = int(input('Bottle size in (ml): '))
-current = int(input('Current nicotine concentration of E-liquid (mg/ml): '))
-nic_base = int(input('Nicotine base strength (mg/ml): '))
-target = int(input('Desired nicotine strength (mg/ml): '))
+def calculate():
 
-# Calculate what the new target should be from user input
-target_converted = target - current
+    amount = int(input('Bottle size in (ml): '))
+    current = int(input('Current nicotine concentration of E-liquid (mg/ml): '))
+    nic_base = int(input('Nicotine base strength (mg/ml): '))
+    target = int(input('Desired nicotine strength (mg/ml): '))
 
-if current == 0:
-    print(' ({}ml bottle) / ({}mg/ml nicotine strength) x ({}mg/ml nicotine target strength) = '\
+    # Calculate what the new target should be from user input
+    target_converted = target - current
+
+    # Run calculation
+    if current == 0:
+      print(' ({}ml bottle) / ({}mg/ml nicotine strength) x ({}mg/ml nicotine target strength) = '\
         .format(amount, nic_base, target))
-    print(amount / nic_base * target,'ml')
-else:
-    print('You entered a current nicotine level of {}mg and target of {}mg, gap in \'mg\' is {}mg ' \
+      no_base = round(amount / nic_base * target, 2)
+      print(no_base,'ml')
+    else:
+      print('You entered a current nicotine level of {}mg and target of {}mg, gap in \'mg\' is {}mg ' \
         .format(current, target, target_converted))
-    print(' ({}ml bottle) / ({}mg/ml nicotine strength) x ({}mg/ml nicotine target strength) = '\
+      print(' ({}ml bottle) / ({}mg/ml nicotine strength) x ({}mg/ml nicotine target strength) = '\
         .format(amount, nic_base, target_converted))
-    print(amount / nic_base * target_converted, 'ml')
+      with_base = round(amount / nic_base * target_converted, 2)
+      print(with_base,'ml')
+
+if __name__ == '__main__':
+    calculate()
+#       print(amount / nic_base * target,'ml')
